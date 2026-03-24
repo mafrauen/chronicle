886517594ba6e27aad00219d267928a4d9b2eda0
Michael Frauenholtz <michael@frauenholtz.com>
2026-03-24 11:05:05 -0500

    Initial Commit

---
+++ ToJo/ToJoApp.swift
@@ -0,0 +1,32 @@
+//
+//  ToJoApp.swift
+//  ToJo
+//
+//  Created by Michael Frauenholtz on 3/24/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ToJoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ToJoTests/ToJoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToJoTests.swift
+//  ToJoTests
+//
+//  Created by Michael Frauenholtz on 3/24/26.
+//
+
+import Testing
+
+struct ToJoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
